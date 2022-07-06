c73732b1643ea826673143834669dcb971874451
James Dinan <jdinan@nvidia.com>
2022-07-06 06:57:08 -0700

    Temporary WAR for Slingshot provider linkspeed bug
    The Slingshot provider underreports the link speed by a factor of 1,000. This
    is a temporary workaround to fix it.
    
    Signed-off-by: James Dinan <jdinan@nvidia.com>

--- src/nccl_ofi_net.c
+++ src/nccl_ofi_net.c
@@ -1585,6 +1585,8 @@ static ncclResult_t ofi_getProperties(int dev, ncclNetProperties_t *props)
 
 	/* Speed reported in Mbps */
 	dev_props.speed = nic_info->link_attr->speed / (1e6);
+	/* Temporary workaround to Slingshot provider link speed bug */
+	if (dev_props.speed < 1000) dev_props.speed *= 1000;
 
 	goto exit;
 
